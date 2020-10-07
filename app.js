e6032059d7c3afe69e14ce162f903753d7f25a6a
hassan haroon <hassanharoon321@gmail.com>
2020-10-08 01:52:34 +0500

    still remain 6 videos now I learn callback functions and first class functions and also who to return functions

--- app.js
+++ app.js
@@ -237,7 +237,7 @@ var jane = Object.create(personProto,{
     job: {value: "teacher"}
 })
 
-//Primitives vs objects(video 10)
+                                            //Primitives vs objects(video 10)
 
 //primitives
 var a = 23;
@@ -271,6 +271,132 @@ function change(a,b){
 change(age,obj);
 console.log(age)
 console.log(obj.city)
+
+
+//commit one end
+
+                                //First Class Functions passing Functions as Argument(video 11)
+
+// 1) A functions is an instance of the object
+// 2) A behaves likes any others objects
+// 3) We can store functions in a variables
+// 4) We can pass a function as an argument to another function
+// 5) We can return a function from a function 
+
+var years = [1990,1965,1937,2005,1998];
+
+function arrayCalc(arr,fn){
+    var arrRes = [];
+    for(var i =0;i<arr.length;i++){
+        arrRes.push(fn(arr[i]));
+    }
+    return arrRes;
+}
+
+function calculateAge(el){
+    return 2020 - el;
+}
+
+function isFullAge(el){
+    return el >= 18;
+}
+
+function maxHeartRate(el){
+    if(el>=18 && el<=81){
+    return Math.round(206.9 - (0.67 * el)); 
+    }
+    else{
+        return -1;
+    }
+}
+
+var ages = arrayCalc(years,calculateAge);
+
+var fullAges = arrayCalc(ages,isFullAge);
+
+var rates = arrayCalc(ages,maxHeartRate)
+
+console.log(ages);
+console.log(fullAges);
+console.log(rates);
+
+//First Class Functions
+
+var years = [1990,1965,1937,2005,1998];
+
+function arrayCalc(arr,fn){
+    var arrRes = [];
+    for(var i =0;i< arr.length;i++){
+        arrRes.push(fn(arr[i]));
+    }
+    return arrRes;
+}
+
+function calculateAge(el){
+    return 2020 - el;
+}
+
+function isFullAge(el){
+    return el >=18;
+}
+
+function maxHeartRate(el){
+    if(el>=18 && el<=81){
+    return Math.round(206.9 -(0.67*el));
+    }
+    else{
+        return -1;
+    }
+}
+
+var ages = arrayCalc(years,calculateAge);
+var fullAge = arrayCalc(ages,isFullAge);
+var rates = arrayCalc(ages,maxHeartRate);
+
+console.log(ages);
+console.log(fullAge);
+console.log(rates);
+
+
+ //First Class Functions Functions Returning Functions(video 12)
+
+function interviewQuestion(job){
+    if(job==="designer"){
+        return function(name){
+            console.log(name+ ", Can you please explain what UX design is?")
+        }
+        
+    }
+    else if(job==="teacher"){
+        return function(name){
+            console.log("What subject do you teach "+name)
+        }
+    }
+    else{
+        return function(name){
+            console.log("Hello "+name+" What do you do")
+        }
+    }
+}
+
+var teacherQuestion = interviewQuestion("teacher");
+var designerQuestion = interviewQuestion("designer");
+var ellse = interviewQuestion("driver");
+
+ellse("khan")
+teacherQuestion("hassan");
+designerQuestion("john");
+
+designerQuestion("siddique")
+designerQuestion("Haroon")
+teacherQuestion("Badden");
+
+//new way to assign
+interviewQuestion("teacher")("hassan")
+
+
+
+
 
 
 
